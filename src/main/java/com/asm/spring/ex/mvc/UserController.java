6829459389edec795d7c787ff5134f96295f7b42
asm <megait@gamil.com>
2024-08-14 20:14:55 +0900

    [안성민] - mvc학습 - 2024/08/14

--- src/main/java/com/asm/spring/ex/mvc/UserController.java
+++ src/main/java/com/asm/spring/ex/mvc/UserController.java
@@ -4,18 +4,53 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.asm.spring.ex.mvc.domain.User;
 import com.asm.spring.ex.mvc.service.UserService;
 
 @Controller
+@RequestMapping("/mvc/user")
 public class UserController
 {
 	@Autowired
 	private UserService userService;
 	
+	//사용자 정보 저장 기능
+	@PostMapping("/create")
+	public String createUser(
+			@RequestParam("name") String name
+			, @RequestParam("birthday") String birthday
+			, @RequestParam("email") String email
+			, @RequestParam("introduce") String introduce
+			, Model model)
+	{
+		//int count = userService.addUser(name, birthday, email, introduce);
+		
+		User user = new User();
+		user.setName(name);
+		user.setYyyymmdd(birthday);
+		user.setEmail(email);
+		user.setIntroduce(introduce);
+		
+		userService.addUserByObject(user);
+		
+		model.addAttribute("result", user);
+		
+		return "mvc/userInfo";
+	}
+	
+	// 입력 화면 페이지
+	@GetMapping("/input")
+	public String userInput()
+	{
+		return "mvc/userinput";
+	}
+	
 	//@RequestMapping(path="/mvc/user/info", method=RequestMethod.GET)
-	@GetMapping("/mvc/user/info")
+	@GetMapping("/info")
 	public String userInfo(Model model)
 	{
 		User user = userService.getLastUser();
--- src/main/java/com/asm/spring/ex/mvc/repository/UserRepository.java
+++ src/main/java/com/asm/spring/ex/mvc/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.asm.spring.ex.mvc.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.asm.spring.ex.mvc.domain.User;
 
@@ -9,4 +10,12 @@ public interface UserRepository
 {
 	// 가장 최근에 조회된 행 조회
 	public User selectLastUser();
+	
+	public int insertUser(
+			@Param("name") String name
+			, @Param("birthday") String birthday
+			, @Param("email") String email
+			, @Param("introduce") String introduce);
+	
+	public int insertUserByObject(User user);
 }
